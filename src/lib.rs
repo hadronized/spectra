7d98d13950eba6a8fe7244c921ae5d75509d33b4
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-08-23 03:06:02 +0200

    window.rs -> bootstrap.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,18 @@ extern crate nalgebra;
 extern crate openal;
 extern crate vorbis;
 
+#[macro_use]
+pub mod report;
+#[macro_use]
+pub mod resource;
+
 pub mod anim;
+pub mod bootstrap;
 pub mod color;
 pub mod device;
 pub mod entity;
 pub mod objects;
 pub mod projection;
-#[macro_use]
-pub mod report;
-#[macro_use]
-pub mod resource;
 pub mod shader;
 pub mod texture;
 pub mod transform;
-pub mod window;
