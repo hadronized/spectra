40ade8981df196969061b33a51d1a078b3aa4ab6
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-09-07 01:19:20 +0200

    Ça commence à sérieusement me gaver en fait là.

--- src/behavior.rs
+++ src/behavior.rs
@@ -1,32 +1,30 @@
 pub type Time = f32;
 
-pub struct Behavior<'a, A> {
-  act: Box<Fn(Time) -> A + 'a>,
-  switch: Box<Fn(Time) -> Option<Behavior<'a, A>> + 'a>
+pub struct Behavior<'a, A> where A: 'a {
+  closure: Box<for <'b> Fn(Time, &'b Behavior<'a, A>) -> (A, &'b Behavior<'a, A>) + 'a>,
 }
 
 impl<'a, A> Behavior<'a, A> {
   pub fn new<F>(act: F) -> Self where F: Fn(Time) -> A + 'a {
     Behavior {
-      act: Box::new(act),
-      switch: Box::new(|_| None)
+      closure: Box::new(move |t, b| {
+        (act(t), b)
+      })
     }
   }
 
-  pub fn switch<F>(self, switch: F) -> Self where F: Fn(Time) -> Option<Behavior<'a, A>> + 'a {
-    Behavior {
-      switch: Box::new(switch),
-      .. self
-    }
+  pub fn run(&self, t: Time) -> (A, &Self) {
+    (self.closure)(t, self)
   }
 
-  pub fn run(self, t: Time) -> (A, Behavior<'a, A>) {
-    let r = (self.act)(t);
-    let next = (self.switch)(t);
-
-    match next {
-      Some(next) => (r, next),
-      None => (r, self)
-    }
-  }
+  //pub fn switch<F>(self, sw: F) -> Self where F: for <'b> Fn(Time, &'b Self) -> Option<&'a Self> + 'a {
+  //  Behavior {
+  //    closure: Box::new(move |t, b| {
+  //      match sw(t, b) {
+  //        Some(sw_b) => sw_b.run(t),
+  //        None => self.run(t)
+  //      }
+  //    })
+  //  }
+  //}
 }
