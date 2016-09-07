edd4288037ce7cf33a074e31f3d2f9b7317000df
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-09-08 01:03:30 +0200

    Separated hot code from cold one in a more simple way (#3).

--- src/resource.rs
+++ src/resource.rs
@@ -1,46 +1,70 @@
-use notify::{self, RecommendedWatcher, Watcher};
-use std::collections::BTreeMap;
-use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc;
-use std::thread;
+#[cfg(feature = "hot-resource")]
+mod hot {
+  use notify::{self, RecommendedWatcher, Watcher};
+  use std::collections::BTreeMap;
+  use std::path::{Path, PathBuf};
+  use std::sync::{Arc, Mutex};
+  use std::sync::mpsc;
+  use std::thread;
 
-pub struct ResourceManager {
-  watcher: RecommendedWatcher,
-  receivers: Arc<Mutex<BTreeMap<PathBuf, mpsc::Sender<()>>>>
-}
+  pub struct ResourceManager {
+    watcher: RecommendedWatcher,
+    receivers: Arc<Mutex<BTreeMap<PathBuf, mpsc::Sender<()>>>>
+  }
 
-impl ResourceManager {
-  pub fn new<P>(root: P) -> Self where P: AsRef<Path> {
-    let (wsx, wrx) = mpsc::channel();
-    let mut watcher: RecommendedWatcher = Watcher::new(wsx).unwrap();
-    let receivers: Arc<Mutex<BTreeMap<PathBuf, mpsc::Sender<()>>>> = Arc::new(Mutex::new(BTreeMap::new()));
+  impl ResourceManager {
+    pub fn new<P>(root: P) -> Self where P: AsRef<Path> {
+      let (wsx, wrx) = mpsc::channel();
+      let mut watcher: RecommendedWatcher = Watcher::new(wsx).unwrap();
+      let receivers: Arc<Mutex<BTreeMap<PathBuf, mpsc::Sender<()>>>> = Arc::new(Mutex::new(BTreeMap::new()));
 
-    let _ = watcher.watch(root);
+      let _ = watcher.watch(root);
 
-    let receivers_ = receivers.clone();
-    let _ = thread::spawn(move || {
-      for event in wrx.iter() {
-        match event {
-          notify::Event { path: Some(path), op: Ok(notify::op::WRITE) } => {
-            if let Some(sx) = receivers_.lock().unwrap().get(&path) {
-              sx.send(()).unwrap();
-            }
-          },
-          _ => {}
+      let receivers_ = receivers.clone();
+      let _ = thread::spawn(move || {
+        for event in wrx.iter() {
+          match event {
+            notify::Event { path: Some(path), op: Ok(notify::op::WRITE) } => {
+              if let Some(sx) = receivers_.lock().unwrap().get(&path) {
+                sx.send(()).unwrap();
+              }
+            },
+            _ => {}
+          }
         }
-      }
-    });
+      });
 
-    ResourceManager {
-      watcher: watcher,
-      receivers: receivers
+      ResourceManager {
+        watcher: watcher,
+        receivers: receivers
+      }
+    }
+
+    pub fn monitor<P>(&mut self, path: P, sx: mpsc::Sender<()>) where P: AsRef<Path> {
+      let mut receivers = self.receivers.lock().unwrap();
+
+      receivers.insert(path.as_ref().to_path_buf(), sx);
     }
   }
+}
 
-  pub fn monitor<P>(&mut self, path: P, sx: mpsc::Sender<()>) where P: AsRef<Path> {
-    let mut receivers = self.receivers.lock().unwrap();
+#[cfg(not(feature = "hot-resource"))]
+mod cold {
+  use std::path::Path;
+  use std::sync::mpsc;
 
-    receivers.insert(path.as_ref().to_path_buf(), sx);
+  pub struct ResourceManager {}
+
+  impl ResourceManager {
+    pub fn new<P>(_: P) -> Self where P: AsRef<Path> {
+      ResourceManager {}
+    }
+
+    pub fn monitor<P>(&mut self, _: P, _: mpsc::Sender<()>) where P: AsRef<Path> {}
   }
 }
+
+#[cfg(feature = "hot-resource")]
+pub use self::hot::*;
+#[cfg(not(feature = "hot-resource"))]
+pub use self::cold::*;
