21a178f7917580a1527821ef258abff861eb0482
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-09-07 11:56:39 +0200

    Simplified the whole shit.

--- src/behavior.rs
+++ src/behavior.rs
@@ -2,29 +2,13 @@ pub type Time = f32;
 
 pub struct Behavior<'a, A> where A: 'a {
   closure: Box<Fn(Time) -> A + 'a>,
+  next: Option<(Time, &'a Behavior<'a, A>)>
 }
 
 impl<'a, A> Behavior<'a, A> {
-  pub fn new<F>(act: F) -> Self where F: Fn(Time) -> A + 'a {
+  pub fn new<F>(act: F, next: Option<(Time, &'a Self)>) -> Self where F: Fn(Time) -> A + 'a {
     Behavior {
       closure: Box::new(act),
-    }
-  }
-
-  pub fn run(&self, t: Time) -> A {
-    (self.closure)(t)
-  }
-}
-
-pub struct Node<'a, A> where A: 'a {
-  behavior: &'a Behavior<'a, A>,
-  next: Option<(Time, &'a Node<'a, A>)>
-}
-
-impl<'a, A> Node<'a, A> {
-  pub fn new(behavior: &'a Behavior<'a, A>, next: Option<(Time, &'a Self)>) -> Self {
-    Node {
-      behavior: behavior,
       next: next
     }
   }
@@ -33,8 +17,8 @@ impl<'a, A> Node<'a, A> {
     let next = self.next;
 
     match next {
-      Some((t_node, node)) if t >= t_node => node.run(t),
-      _ => (self.behavior.run(t), self)
+      Some((node_t, node)) if t >= node_t => node.run(t),
+      _ => ((self.closure)(t), self)
     }
   }
 }
