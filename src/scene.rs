9e39a156800c2ebad5efec296c95c08132ed05e9
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-10-25 00:29:24 +0200

    String -> &str for get_id in public interface.

--- src/scene.rs
+++ src/scene.rs
@@ -25,20 +25,24 @@ impl<'a> Scene<'a> {
     }
   }
 
-  pub fn get_id<T>(&mut self, inst_name: String) -> Option<Id> where T: GetId {
+  pub fn get_id<T>(&mut self, inst_name: &str) -> Option<Id> where T: GetId {
     T::get_id(self, inst_name)
+  }
+
+  pub fn resource_manager(&mut self) -> &mut ResourceManager {
+    &mut self.res_manager
   }
 }
 
 pub type Id = u32;
 
 pub trait GetId {
-  fn get_id<'a>(scene: &mut Scene<'a>, inst_name: String) -> Option<Id>;
+  fn get_id<'a>(scene: &mut Scene<'a>, inst_name: &str) -> Option<Id>;
 }
 
 impl<'b> GetId for Model<'b> {
-  fn get_id<'a>(scene: &mut Scene<'a>, inst_name: String) -> Option<Id> {
-    match scene.model_cache.get(&inst_name).cloned() {
+  fn get_id<'a>(scene: &mut Scene<'a>, inst_name: &str) -> Option<Id> {
+    match scene.model_cache.get(inst_name).cloned() {
       id@Some(..) => id,
       None => {
         // cache miss; load then
@@ -53,7 +57,7 @@ impl<'b> GetId for Model<'b> {
               // add the model to the list of loaded models
               scene.models.push(model);
               // add the model to the cache
-              scene.model_cache.insert(inst_name, model_id);
+              scene.model_cache.insert(inst_name.to_owned(), model_id);
 
               Some(model_id)
             },
