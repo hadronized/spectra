7a3ad5deb0115db96ab76e0868b53745d8fcdc33
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-10-31 03:02:23 +0100

    More explicite perspective() implementation.

--- src/projection.rs
+++ src/projection.rs
@@ -6,6 +6,6 @@ pub trait Projectable {
 }
 
 pub fn perspective(ratio: f32, fovy: f32, znear: f32, zfar: f32) -> M44 {
-  *Perspective3::new(ratio, fovy, znear, zfar).to_matrix().as_ref()
+  Perspective3::new(ratio, fovy, znear, zfar).to_matrix().as_ref().clone()
 }
 
