dea3a446eecec3353cd23464e2e267c968478f43
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-11-03 03:05:11 +0100

    Migrated Texture to Load+Reload!

--- src/texture.rs
+++ src/texture.rs
@@ -1,9 +1,12 @@
 use luminance::{Dim2, Flat, Sampler};
 use luminance_gl::gl33::Texture;
 use image::{self, ImageResult};
+use std::ops::Deref;
 use std::path::Path;
 
 pub use luminance::RGBA32F;
+
+use resource::{Load, LoadError, Reload};
 
 /// Load an RGBA texture from an image at a path.
 pub fn load_rgba_texture<P>(path: P, sampler: &Sampler, linear: bool) -> ImageResult<Texture<Flat, Dim2, RGBA32F>> where P: AsRef<Path> {
@@ -42,127 +45,36 @@ pub fn save_rgba_texture<P>(texture: &Texture<Flat, Dim2, RGBA32F>, path: P) whe
   let _ = image::save_buffer(path, &output, w, h, image::ColorType::RGBA(8));
 }
 
-#[derive(Clone, Debug)]
-pub enum TextureError {
-  LoadingFailed(String)
+pub struct TextureImage {
+  pub texture: Texture<Flat, Dim2, RGBA32F>,
+  sampler: Sampler,
+  linear: bool,
 }
 
-#[cfg(feature = "hot-resource")]
-mod hot {
-  use luminance::{Dim2, Flat, Sampler};
-  use luminance_gl::gl33::Texture;
-  use std::ops::Deref;
-  use std::path::{Path, PathBuf};
-  use std::sync::mpsc;
+impl Deref for TextureImage {
+  type Target = Texture<Flat, Dim2, RGBA32F>;
 
-  use resource::ResourceManager;
-
-  use super::*;
-
-  pub struct TextureImage {
-    rx: mpsc::Receiver<()>,
-    last_update_time: Option<f64>,
-    texture: Texture<Flat, Dim2, RGBA32F>,
-    sampler: Sampler,
-    linear: bool,
-    path: PathBuf
-  }
-
-  impl TextureImage {
-    pub fn load<P>(manager: &mut ResourceManager, path: P, sampler: &Sampler, linear: bool) -> Result<Self, TextureError> where P: AsRef<Path> {
-      let path = path.as_ref();
-
-      let tex = load_rgba_texture(path, sampler, linear);
-
-      match tex {
-        Ok(tex) => {
-          // monitor that texture
-          let (sx, rx) = mpsc::channel();
-
-          manager.monitor(path, sx);
-
-          Ok(TextureImage {
-            rx: rx,
-            last_update_time: None,
-            texture: tex,
-            sampler: *sampler,
-            linear: linear,
-            path: path.to_path_buf()
-          })
-        },
-        Err(e) => {
-          Err(TextureError::LoadingFailed(format!("{:?}", e)))
-        }
-      }
-    }
-
-    fn reload(&mut self) {
-      let path = self.path.as_path();
-      let tex = load_rgba_texture(path, &self.sampler, self.linear);
-
-      match tex {
-        Ok(tex) => {
-          self.texture = tex;
-          info!("reloaded texture {:?}", path);
-        },
-        Err(e) => {
-          err!("reloading texture {:?} has failed: {:?}", path, e);
-        }
-      }
-    }
-
-    decl_sync_hot!();
-  }
-
-  impl Deref for TextureImage {
-    type Target = Texture<Flat, Dim2, RGBA32F>;
-
-    fn deref(&self) -> &Self::Target {
-      &self.texture
-    }
+  fn deref(&self) -> &Self::Target {
+    &self.texture
   }
 }
 
-#[cfg(not(feature = "hot-resource"))]
-mod cold {
-  use luminance::{Dim2, Flat, Sampler};
-  use luminance_gl::gl33::Texture;
-  use std::ops::Deref;
-  use std::path::Path;
+impl Load for TextureImage {
+  type Args = (Sampler, bool);
 
-  use resource::ResourceManager;
-
-  use super::*;
-
-  pub struct TextureImage(Texture<Flat, Dim2, RGBA32F>);
-
-  impl TextureImage {
-    pub fn load<P>(_: &mut ResourceManager, path: P, sampler: &Sampler, linear: bool) -> Result<Self, TextureError> where P: AsRef<Path> {
-      let tex = load_rgba_texture(path, sampler, linear);
-
-      match tex {
-        Ok(tex) => {
-          Ok(TextureImage(tex))
-        },
-        Err(e) => {
-          Err(TextureError::LoadingFailed(format!("{:?}", e)))
-        }
-      }
-    }
-
-    pub fn sync(&mut self) {}
-  }
-
-  impl Deref for TextureImage {
-    type Target = Texture<Flat, Dim2, RGBA32F>;
-
-    fn deref(&self) -> &Self::Target {
-      &self.0
-    }
+  fn load<P>(path: P, (sampler, linear): Self::Args) -> Result<Self, LoadError> where P: AsRef<Path> {
+    load_rgba_texture(path, &sampler, linear)
+      .map(|tex| TextureImage {
+        texture: tex,
+        sampler: sampler,
+        linear: linear
+        })
+      .map_err(|e| LoadError::ConversionFailed(format!("{:?}", e)))
   }
 }
 
-#[cfg(feature = "hot-resource")]
-pub use self::hot::*;
-#[cfg(not(feature = "hot-resource"))]
-pub use self::cold::*;
+impl Reload for TextureImage {
+  fn reload<P>(&self, path :P) -> Result<Self, LoadError> where P: AsRef<Path> {
+    Self::load(path, (self.sampler, self.linear))
+  }
+}
