8122a3c4dad31d9463e73ad830a838a2f4376b09
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-11-10 03:16:00 +0100

    Better time formatting in logs.

--- src/report.rs
+++ src/report.rs
@@ -1,23 +1,40 @@
 #[macro_export]
+macro_rules! now {
+  () => {{
+    let t = ::time::now();
+    let h = t.tm_hour;
+    
+    format!("{month:0>2}/{day:0>2}/{year} {hour:>2}:{min:0>2}:{secs:0>2}:{nsecs:0>9}",
+            month = t.tm_mon + 1,
+            day = t.tm_mday,
+            year = t.tm_year - 100,
+            hour = if h == 23 { 0 } else { h + 1 },
+            min = t.tm_min,
+            secs = t.tm_sec,
+            nsecs = t.tm_nsec)
+  }}
+}
+
+#[macro_export]
 macro_rules! deb {
-  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[36m> ", $e, "\x1b[0m"), ::time::now().ctime()); };
-  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[36m> ", $e, "\x1b[0m"), ::time::now().ctime(), $($arg)+); };
+  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[36m> ", $e, "\x1b[0m"), now!()); };
+  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[36m> ", $e, "\x1b[0m"), now!(), $($arg)+); };
 }
 
 #[macro_export]
 macro_rules! info {
-  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[34m> ", $e, "\x1b[0m"), ::time::now().ctime()); };
-  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[34m> ", $e, "\x1b[0m"), ::time::now().ctime(), $($arg)+); };
+  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[34m> ", $e, "\x1b[0m"), now!()); };
+  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[34m> ", $e, "\x1b[0m"), now!(), $($arg)+); };
 }
 
 #[macro_export]
 macro_rules! warn {
-  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[33m> ", $e, "\x1b[0m"), ::time::now().ctime()); };
-  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[33m> ", $e, "\x1b[0m"), ::time::now().ctime(), $($arg)+); };
+  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[33m> ", $e, "\x1b[0m"), now!()); };
+  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[33m> ", $e, "\x1b[0m"), now!(), $($arg)+); };
 }
 
 #[macro_export]
 macro_rules! err {
-  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[1;31m> ", $e, "\x1b[0;0m"), ::time::now().ctime()); };
-  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[1;31m> ", $e, "\x1b[0;0m"), ::time::now().ctime(), $($arg)+); };
+  ( $e:expr ) => { println!(concat!("\x1b[90m{} \x1b[1;31m> ", $e, "\x1b[0;0m"), now!()); };
+  ( $e:expr, $($arg:tt)+ ) => { println!(concat!("\x1b[90m{} \x1b[1;31m> ", $e, "\x1b[0;0m"), now!(), $($arg)+); };
 }
