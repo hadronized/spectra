417c6e4876a060c88b5f322e1eab17ba506bc107
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-11-11 01:27:48 +0100

    impl Load for Object.

--- src/object.rs
+++ src/object.rs
@@ -1,7 +1,11 @@
-use nalgebra::ToHomogeneous;
+use nalgebra::{Quaternion, ToHomogeneous, Unit};
+use serde_json::from_reader;
+use std::path::Path;
+use std::fs::File;
 
 use id::Id;
 use model::Model;
+use resource::{Cache, Get, Load, LoadError};
 use transform::{M44, Orientation, Position, Scale, Transformable, translation_matrix};
 
 pub struct Object<'a> {
@@ -24,4 +28,30 @@ pub struct ObjectManifest {
   position: [f32; 3],
   orientation: [f32; 4],
   scale: [f32; 3]
+}
+
+impl<'a> Load<'a> for Object<'a> {
+  type Args = ();
+
+  fn load<P>(path: P, cache: &mut Cache<'a>, _: Self::Args) -> Result<Self, LoadError> where P: AsRef<Path> {
+    let path = path.as_ref();
+
+    info!("loading object {:?}", path);
+
+    // read the manifest
+    let manifest: ObjectManifest = {
+      let file = File::open("data/scene/objects/{}.json").map_err(|e| LoadError::FileNotFound(path.to_path_buf(), format!("{:?}", e)))?;
+      from_reader(file).map_err(|e| LoadError::ParseFailed(format!("{:?}", e)))?
+    };
+
+    // get the model id
+    let model_id = cache.get_id(&manifest.model, ()).ok_or(LoadError::ConversionFailed(format!("unable to find model {} for object at {:?}", manifest.model, path)))?;
+
+    Ok(Object {
+      model: model_id,
+      position: (&manifest.position).into(),
+      orientation: Unit::new(&Quaternion::from(&manifest.orientation)),
+      scale: (&manifest.scale).into()
+    })
+  }
 }
