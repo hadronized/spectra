e7aec9d45ae8262b91aa4a3ef7671b9ae2667488
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-11-23 03:00:09 +0100

    DefaultProgram -> DefaultProgram2D.

--- src/extra/shaders/default.rs
+++ src/extra/shaders/default.rs
@@ -11,9 +11,9 @@ use scene::Scene;
 pub type ColorUniform = Uniform<[f32; 4]>;
 pub const DEFAULT_2D_COLOR: ColorUniform = Uniform::new(0);
 
-pub struct DefaultProgram<'a>(Id<'a, Program>);
+pub struct DefaultProgram2D<'a>(Id<'a, Program>);
 
-impl<'a> Deref for DefaultProgram<'a> {
+impl<'a> Deref for DefaultProgram2D<'a> {
   type Target = Id<'a, Program>;
 
   fn deref(&self) -> &Self::Target {
@@ -21,8 +21,8 @@ impl<'a> Deref for DefaultProgram<'a> {
   }
 }
 
-impl<'a> DefaultProgram<'a> {
+impl<'a> DefaultProgram2D<'a> {
   pub fn get_from(scene: &mut Scene<'a>) -> Option<Self> {
-    get_id!(scene, "default.glsl", vec![ColorUniform::sem("color")]).map(DefaultProgram)
+    get_id!(scene, "default_2d.glsl", vec![ColorUniform::sem("color")]).map(DefaultProgram)
   }
 }
