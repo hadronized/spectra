70a051e687f1bb99c772b1a780e0d7fa5ace2b12
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-11-25 03:00:38 +0100

    Added the 3D default shader.

---
+++ spectra/data/shaders/default_3d.glsl
@@ -0,0 +1,37 @@
+#vs
+
+layout (location = 0) in vec3 co;
+layout (location = 1) in vec3 no;
+
+out vec3 v_p;
+out vec3 v_no;
+
+uniform mat4 proj;
+uniform mat4 view;
+uniform mat4 inst;
+
+void main() {
+  mat4 view_inst = view * inst;
+  mat4 normal_mat = transpose(inverse(view_inst);
+
+  gl_Position = proj * view_inst * vec4(co, 1.);
+
+  v_p = inst * vec4(co, 1.);
+  v_no = normal_mat * vec4(no, 1.);
+}
+
+#fs
+
+in vec3 v_p;
+in vec3 v_no;
+
+out vec4 frag;
+
+uniform vec3 color;
+
+void main() {
+  vec3 light_dir = vec3(0., 1., 0.);
+  float kd = max(0., dot(v_no, light_dir));
+
+  frag = color * kd;
+}
--- src/extra/shaders/default.rs
+++ src/extra/shaders/default.rs
@@ -1,6 +1,6 @@
 //! Default shaders.
 
-use luminance::Sem;
+use luminance::{M44, Sem};
 use luminance_gl::gl33::Uniform;
 use std::ops::Deref;
 
@@ -9,6 +9,8 @@ use shader::Program;
 use scene::Scene;
 
 pub type ColorUniform = Uniform<[f32; 4]>;
+pub type Mat44Uniform = Uniform<M44>;
+
 pub const DEFAULT_2D_COLOR: ColorUniform = Uniform::new(0);
 
 pub struct DefaultProgram2D<'a>(Id<'a, Program>);
@@ -24,5 +26,31 @@ impl<'a> Deref for DefaultProgram2D<'a> {
 impl<'a> DefaultProgram2D<'a> {
   pub fn get_from(scene: &mut Scene<'a>) -> Option<Self> {
     get_id!(scene, "default_2d.glsl", vec![ColorUniform::sem("color")]).map(DefaultProgram2D)
+  }
+}
+
+pub const DEFAULT_3D_PROJ: Mat44Uniform = Uniform::new(0);
+pub const DEFAULT_3D_VIEW: Mat44Uniform = Uniform::new(1);
+pub const DEFAULT_3D_INST: Mat44Uniform = Uniform::new(2);
+pub const DEFAULT_3D_COLOR: ColorUniform = Uniform::new(3);
+
+pub struct DefaultProgram3D<'a>(Id<'a, Program>);
+
+impl<'a> Deref for DefaultProgram3D<'a> {
+  type Target = Id<'a, Program>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl<'a> DefaultProgram3D<'a> {
+  pub fn get_from(scene: &mut Scene<'a>) -> Option<Self> {
+    get_id!(scene, "default_3d.glsl", vec![
+              Mat44Uniform::sem("proj"),
+              Mat44Uniform::sem("view"),
+              Mat44Uniform::sem("inst"),
+              ColorUniform::sem("color")])
+      .map(DefaultProgram3D)
   }
 }
