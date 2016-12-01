6878290d542d72ff8465081d640467f186088e66
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-12-01 03:05:44 +0100

    Enhance help output of spectra bin.

--- spectra/src/main.rs
+++ spectra/src/main.rs
@@ -1,6 +1,7 @@
+#[macro_use]
 extern crate clap;
 
-use clap::{App, SubCommand};
+use clap::{App, AppSettings, SubCommand};
 use std::fs::{File, create_dir_all};
 use std::io::Write;
 use std::path::Path;
@@ -9,6 +10,10 @@ include!(concat!(env!("OUT_DIR"), "/resources.rs"));
 
 fn main() {
   let options = App::new("spectra")
+    .version(crate_version!())
+    .author(crate_authors!())
+    .about("Spectra engine utility")
+    .setting(AppSettings::SubcommandRequired)
     .subcommand(SubCommand::with_name("bootstrap")
          .about("Create default resources in your end-user project"))
     .get_matches();
