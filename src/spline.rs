280b355885be4b55c92ff6116764e7d1504f0c2f
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-02-10 03:11:59 +0100

    Doc spline.rs.

--- src/spline.rs
+++ src/spline.rs
@@ -13,7 +13,7 @@ pub type Time = f32;
 
 /// A spline control point.
 ///
-/// This type associates a value at a given type. It also contains an interpolation object used to
+/// This type associates a value at a given time. It also contains an interpolation object used to
 /// determine how to interpolate values on the segment defined by this key and the next one.
 #[derive(Copy, Clone, Debug, Deserialize, Serialize)]
 pub struct Key<T> {
@@ -27,6 +27,7 @@ pub struct Key<T> {
 }
 
 impl<T> Key<T> {
+  /// Create a new key.
   pub fn new(t: Time, value: T, interpolation: Interpolation) -> Self {
     Key {
       t: t,
@@ -59,6 +60,7 @@ pub enum Interpolation {
 }
 
 impl Default for Interpolation {
+  /// `Interpolation::Linear` is the default.
   fn default() -> Self {
     Interpolation::Linear
   }
@@ -71,11 +73,13 @@ pub struct Spline<T> {
 }
 
 impl<T> Spline<T> {
-  pub fn new(mut cps: Vec<Key<T>>) -> Self {
-    cps.sort_by(|k0, k1| k0.t.partial_cmp(&k1.t).unwrap());
+  /// Create a new spline out of keys. The keys don’t have to be sorted because they’re sorted by
+  /// this function.
+  pub fn from_keys(mut keys: Vec<Key<T>>) -> Self {
+    keys.sort_by(|k0, k1| k0.t.partial_cmp(&k1.t).unwrap());
 
     Spline {
-      keys: cps
+      keys: keys 
     }
   }
 }
@@ -91,11 +95,11 @@ impl<'a, T> Load<'a> for Spline<T> where T: Deserialize {
     let file = File::open(path).map_err(|e| LoadError::FileNotFound(path.to_path_buf(), format!("{:?}", e)))?;
     let keys = from_reader(file).map_err(|e| LoadError::ParseFailed(format!("{:?}", e)))?;
 
-    Ok(Spline::new(keys))
+    Ok(Spline::from_keys(keys))
   }
 }
 
-
+/// Iterator over spline keys.
 pub struct SplineIterator<'a, T> where T: 'a {
   anim_param: &'a Spline<T>,
   i: usize
@@ -127,11 +131,14 @@ impl<'a, T> IntoIterator for &'a Spline<T> {
   }
 }
 
-/// Implement this trait if your type is a key you want to sample with.
+/// Keys that can be interpolated between. Implementing this trait is required to perform sampling
+/// on splines.
 pub trait Interpolate: Copy {
   /// Linear interpolation.
   fn lerp(a: Self, b: Self, t: Time) -> Self;
   /// Cubic hermite interpolation.
+  ///
+  /// Default to `Self::lerp`.
   fn cubic_hermite(_: (Self, Time), a: (Self, Time), b: (Self, Time), _: (Self, Time), t: Time) -> Self {
     Self::lerp(a.0, b.0, t)
   }
@@ -204,61 +211,74 @@ pub fn cubic_hermite<T>(x: (T, Time), a: (T, Time), b: (T, Time), y: (T, Time), 
   a.0 * (two_t3 - three_t2 + 1.) + m0 * (t3 - 2. * t2 + t) + b.0 * (-two_t3 + three_t2) + m1 * (t3 - t2)
 }
 
-/// Samplers can sample `Spline` by providing a `Time`. They should be mutable so that they can
-/// maintain an internal state for optimization purposes.
-#[derive(Copy, Clone, Default)]
-pub struct Sampler {
+/// Samplers can sample a spline by providing time. They should be mutable so that they can maintain
+/// an internal state for optimization purposes.
+#[derive(Copy, Clone)]
+pub struct Sampler<'a, T> where T: 'a {
+  /// Borrowed spline.
+  spline: &'a Spline<T>,
   /// Playback cursor – gives the lower control point index of the current portion of the curve
   /// we’re sampling at.
   cursor: usize
 }
 
-impl Sampler {
-  pub fn new() -> Self {
+impl<'a, T> Sampler<'a, T> where T: 'a {
+  pub fn new(spline: &'a Spline<T>) -> Self {
     Sampler {
+      spline: spline,
       cursor: 0
     }
   }
 
-  /// Sample a spline. If `random_sampling` is set, random sampling is generally faster than
-  /// continuous sampling. Though, if you use continuous sampling, set `random_sampling` to `false`
-	/// for max speed performance.
-  pub fn sample<T>(&mut self, t: Time, param: &Spline<T>, random_sampling: bool) -> Option<T>
+  /// Sample a spline at a given time.
+  ///
+  /// The spline might be altered for optimization purpose, in a way so that next calls to this
+  /// function have few work to do.
+  ///
+  /// # Return
+  ///
+  /// `None` if you try to sample a value at a time that has no key associated with. That can also
+  /// happen if you try to sample between two keys with a specific interpolation mode that make the
+  /// sampling impossible. For instance, `Interpolate::CatmullRom` requires *four* keys. If you’re
+  /// near the beginning of the spline or its end, ensure you have enough keys around to make the
+  /// sampling.
+  ///
+  /// # Complexity
+  ///
+  /// The idea is that if you pass in a continuous ascending time, this function gives a response in
+  /// a constant time – *O(1)*. If you pass a stochastic or arbitrary time (for instance for
+  /// debugging or for editing purposes), the complexity is linear – *O(N)* on the number of keys.
+  pub fn sample(&mut self, t: Time) -> Option<T>
       where T: Interpolate {
-    let i = if random_sampling {
-      binary_search_lower_cp(&param.keys, t)
-    } else {
-      let i = around_search_lower_cp(&param.keys, self.cursor, t);
+    let keys = &self.spline.keys;
+    let i = around_search_lower_cp(keys, self.cursor, t);
 
-      // if we’ve found the index, replace the cursor to speed up next searches
-      if let Some(cursor) = i {
-        self.cursor = cursor;
-      }
-
-      i
-    };
+    // if we’ve found the index, replace the cursor to speed up next searches
+    if let Some(cursor) = i {
+      self.cursor = cursor;
+    }
 
     let i = match i {
       Some(i) => i,
       None => return None
     };
 
-    let cp0 = &param.keys[i];
+    let cp0 = &keys[i];
 
     match cp0.interpolation {
       Interpolation::Step(threshold) => {
-        let cp1 = &param.keys[i+1];
+        let cp1 = &keys[i+1];
         let nt = normalize_time(t, cp0, cp1);
         Some(if nt < threshold { cp0.value } else { cp1.value })
       },
       Interpolation::Linear => {
-        let cp1 = &param.keys[i+1];
+        let cp1 = &keys[i+1];
         let nt = normalize_time(t, cp0, cp1);
 
         Some(Interpolate::lerp(cp0.value, cp1.value, nt))
       },
       Interpolation::Cosine => {
-        let cp1 = &param.keys[i+1];
+        let cp1 = &keys[i+1];
         let nt = normalize_time(t, cp0, cp1);
         let cos_nt = (1. - f32::cos(nt * consts::PI)) * 0.5;
 
@@ -267,12 +287,12 @@ impl Sampler {
       Interpolation::CatmullRom => {
         // We need at least four points for Catmull Rom; ensure we have them, otherwise, return
         // None.
-        if i == 0 || i >= param.keys.len() - 2 {
+        if i == 0 || i >= keys.len() - 2 {
           None
         } else {
-          let cp1 = &param.keys[i+1];
-          let cpm0 = &param.keys[i-1];
-          let cpm1 = &param.keys[i+2];
+          let cp1 = &keys[i+1];
+          let cpm0 = &keys[i-1];
+          let cpm1 = &keys[i+2];
           let nt = normalize_time(t, cp0, cp1);
 
           Some(Interpolate::cubic_hermite((cpm0.value, cpm0.t), (cp0.value, cp0.t), (cp1.value, cp1.t), (cpm1.value, cpm1.t), nt))
