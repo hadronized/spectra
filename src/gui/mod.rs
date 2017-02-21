32859ea09948fefa4904a91c935b1fb70b6d55d1
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-02-21 04:04:26 +0100

    Click implemented in the GUI, and passed down to widgets.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -24,7 +24,9 @@ pub struct GUI<'a> {
   widgets: HashMap<String, Box<Widget<'a> + 'a>>,
 
   // event stuff
-  last_cursor: [f64; 2]
+  last_cursor: Option<[f64; 2]>,
+  last_mouse_left_down: Option<[f64; 2]>,
+  last_mouse_left_up: Option<[f64; 2]>
 }
 
 impl<'a> GUI<'a> {
@@ -32,7 +34,9 @@ impl<'a> GUI<'a> {
     GUI {
       renderer: Renderer::new(viewport.w.ceil() as u32, viewport.h.ceil() as u32, 1024, 1024, 1024, scene),
       widgets: HashMap::new(),
-      last_cursor: [0., 0.]
+      last_cursor: None,
+      last_mouse_left_down: None,
+      last_mouse_left_up: None
     }
   }
 
@@ -51,7 +55,7 @@ impl<'a> GUI<'a> {
     let mut texts = Vec::new();
 
     for widget in self.widgets.values() {
-      for prim in (*widget).unwidget() {
+      for prim in widget.unwidget() {
         match prim {
           WidgetPrim::Triangle(ref tri) => tris.push(*tri),
           WidgetPrim::Quad(ref quad) => quads.push(*quad),
@@ -73,12 +77,37 @@ impl<'a> GUI<'a> {
 
 impl<'a> EventHandler for GUI<'a> {
   fn on_mouse_button(&mut self, button: MouseButton, action: Action) -> bool {
+    if let MouseButton::Button1 = button {
+      let last_cursor = self.last_cursor.unwrap();
+
+      match action {
+        Action::Press => {
+          self.last_mouse_left_down = Some(last_cursor);
+          self.last_mouse_left_up = None;
+        },
+        Action::Release => {
+          // check whether it’s a click
+          if px_dist(self.last_mouse_left_down.unwrap(), last_cursor) <= 5. {
+            // it’s a click
+            for widget in self.widgets.values() {
+              widget.on_click(last_cursor);
+            }
+          }
+
+          self.last_mouse_left_down = None;
+          self.last_mouse_left_up = Some(last_cursor);
+
+        },
+        _ => ()
+      }
+    }
+
     true
   }
 
   // TODO: change the implementation to take into account widget focus
   fn on_cursor_move(&mut self, cursor: [f64; 2]) -> bool {
-    self.last_cursor = cursor;
+    self.last_cursor = Some(cursor);
     true
   }
 }
@@ -96,6 +125,7 @@ pub enum WidgetPrim<'a> {
 
 pub trait Widget<'a> {
   fn unwidget(&self) -> Vec<WidgetPrim<'a>>;
+  fn on_click(&self, cursor: [f64; 2]);
 }
 
 pub struct ProgressBar {
@@ -163,6 +193,20 @@ impl ProgressBar {
       l.borrow_mut().on(ProgressBarEvent::Toggle);
     }
   }
+
+  pub fn on_click(&mut self, cursor: [f64; 2]) {
+    let c = cursor[0] as f32;
+
+    // update the quads
+    self.progress_quad.0.pos[0] = c;
+    self.progress_quad.1.pos[0] = c;
+    self.inactive_quad.2.pos[0] = c;
+    self.inactive_quad.3.pos[0] = c;
+
+    for l in &self.listeners {
+      l.borrow_mut().on(ProgressBarEvent::Set(c / (self.recip_dur_sec * self.w)));
+    }
+  }
 }
 
 impl<'a> Widget<'a> for Rc<RefCell<ProgressBar>> {
@@ -170,4 +214,16 @@ impl<'a> Widget<'a> for Rc<RefCell<ProgressBar>> {
     let bar = &self.borrow();
     vec![WidgetPrim::Quad(bar.progress_quad), WidgetPrim::Quad(bar.inactive_quad)]
   }
+
+  fn on_click(&self, cursor: [f64; 2]) {
+    self.borrow_mut().on_click(cursor);
+  }
+}
+
+/// Pixel distance between two points.
+fn px_dist(a: [f64; 2], b: [f64; 2]) -> f64 {
+  let x = b[0] - a[0];
+  let y = b[1] - a[1];
+
+  f64::sqrt(x*x + y*y)
 }
