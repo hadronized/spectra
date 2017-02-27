1c00d8e2a15d54429ee8b353df63f206d0fb8fcc
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-02-27 03:15:42 +0100

    Simplified the Forward compositor.

--- src/extra/compositors/forward.rs
+++ src/extra/compositors/forward.rs
@@ -10,10 +10,9 @@ pub type Texture2D<A> = Texture<Flat, Dim2, A>;
 const FORWARD_SOURCE: &'static Uniform<Unit> = &Uniform::new(0);
 
 pub struct Forward {
+  framebuffer: Framebuffer<Flat, Dim2, (), ()>,
   program: Res<Program>,
-  quad: Tess,
-  w: u32,
-  h: u32
+  quad: Tess
 }
 
 impl Forward {
@@ -21,19 +20,17 @@ impl Forward {
     let program = scene.get("spectra/compositors/forward.glsl", vec![FORWARD_SOURCE.sem("source")]).unwrap();
 
     Forward {
+      framebuffer: Framebuffer::default((w, h)),
       program: program,
       quad: Tess::attributeless(Mode::TriangleStrip, 4),
-      w: w,
-      h: h
     }
   }
 
   pub fn to_screen(&self, source: &Texture2D<RGBA32F>) {
-    let back_fb = Framebuffer::default((self.w, self.h));
     let textures: &[&RawTexture] = &[source];
     let tess_render = TessRender::one_whole(&self.quad);
 
-    Pipeline::new(&back_fb, [0., 0., 0., 0.], textures, &[], vec![
+    Pipeline::new(&self.framebuffer, [0., 0., 0., 0.], textures, &[], vec![
       Pipe::empty()
         .uniforms(&[FORWARD_SOURCE.alter(Unit::new(0))])
         .unwrap(ShadingCommand::new(&self.program.borrow(), vec![
@@ -44,7 +41,6 @@ impl Forward {
   }
 
   pub fn black_screen(&self) {
-    let back_fb = Framebuffer::default((self.w, self.h));
-    Pipeline::new(&back_fb, [0., 0., 0., 0.], &[], &[], vec![]).run();
+    Pipeline::new(&self.framebuffer, [0., 0., 0., 0.], &[], &[], vec![]).run();
   }
 }
