f9f94ff1ea860905709f6dd6823bd10672c2bc93
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-03-07 17:07:03 +0100

    Implemented parts of the Compositor.

--- src/compositing.rs
+++ src/compositing.rs
@@ -1,5 +1,6 @@
-use luminance::{Depth32F, Dim2, Flat, Framebuffer, Mode, RGBA32F, RenderCommand, Tess, Texture,
-                Uniform, Unit};
+use luminance::{Depth32F, Dim2, Flat, Framebuffer, Mode, RGBA32F, Tess, Texture, Uniform, Unit};
+use luminance::pipeline::{Pipe, Pipeline, RenderCommand, ShadingCommand};
+use luminance::tess::TessRender;
 use std::ops::{Add, Sub, Mul};
 
 pub use luminance::{Equation, Factor};
@@ -8,24 +9,39 @@ use resource::Res;
 use scene::Scene;
 use shader::Program;
 
-/// Layer render that can be embedded into a compositing graph.
-pub type LayerRender<'a> = (&'a ColorMap, &'a DepthMap);
 /// Simple texture that can be embedded into a compositing graph.
-pub type LayerTexture<'a> = &'a ColorMap;
+pub type TextureLayer<'a> = &'a ColorMap;
 
 pub type ColorMap = Texture<Flat, Dim2, RGBA32F>;
 pub type DepthMap = Texture<Flat, Dim2, Depth32F>;
+
+/// Render layer used to host renders.
+pub struct RenderLayer<'a> {
+  shading_commands: Vec<Pipe<'a, ShadingCommand<'a>>>
+}
+
+impl<'a> RenderLayer<'a> {
+  pub fn new() -> Self {
+    RenderLayer {
+      shading_commands: Vec::new()
+    }
+  }
+
+  pub fn push_shading_command(&mut self, shading_command: Pipe<'a, ShadingCommand<'a>>) {
+    self.shading_commands.push(shading_command);
+  }
+}
 
 /// Compositing node.
 pub enum Node<'a> {
   /// A render node.
   ///
-  /// Contains a color map and a depth map.
-  Render(LayerRender<'a>),
+  /// Contains render layer.
+  Render(RenderLayer<'a>),
   /// A texture node.
   ///
   /// Contains a single texture.
-  Texture(LayerTexture<'a>),
+  Texture(TextureLayer<'a>),
   /// Composite node.
   ///
   /// Composite nodes are used to blend two compositing nodes according to a given `Equation` and
@@ -40,14 +56,14 @@ impl<'a> Node<'a> {
   }
 }
 
-impl<'a> From<LayerRender<'a>> for Node<'a> {
-  fn from(layer: LayerRender<'a>) -> Self {
+impl<'a> From<RenderLayer<'a>> for Node<'a> {
+  fn from(layer: RenderLayer<'a>) -> Self {
     Node::Render(layer)
   }
 }
 
-impl<'a> From<LayerTexture<'a>> for Node<'a> {
-  fn from(texture: LayerTexture<'a>) -> Self {
+impl<'a> From<TextureLayer<'a>> for Node<'a> {
+  fn from(texture: TextureLayer<'a>) -> Self {
     Node::Texture(texture)
   }
 }
@@ -86,9 +102,9 @@ pub struct Compositor {
   framebuffers: Vec<Framebuffer<Flat, Dim2, ColorMap, DepthMap>>,
   // free list of available framebuffers
   free_framebuffers: Vec<usize>,
-  // program used to render nodes
+  // program used to compose nodes
   program: Res<Program>,
-  // fullscreen quad for compositing
+  // attributeless fullscreen quad for compositing
   quad: Tess
 }
 
@@ -120,36 +136,99 @@ impl Compositor {
     })
   }
 
-  /// Whenever a node has finished being composed, we *might* need to release the framebuffer it has
+  /// Whenever a node has finished being composed, we *might* need to dispose the framebuffer it has
   /// pulled. This funciton does that job.
   ///
-  /// It never deallocated memory. It has an important property: once a framebuffer is pulled,
+  /// It never deallocates memory. It has an important property: once a framebuffer is pulled,
   /// calling that function will make it available for other nodes, improving memory usage for the
   /// next calls.
-  fn release_framebuffer(&mut self, framebuffer_index: usize) {
+  #[inline]
+  fn dispose_framebuffer(&mut self, framebuffer_index: usize) {
     self.free_framebuffers.push(framebuffer_index);
   }
 
   /// Consume and display a compositing graph represented by its nodes.
   pub fn display(&mut self, root: Node) {
-    self.consume_tagged_nodes(TaggedNode { node: root, framebuffer_index: None });
+    unimplemented!()
+    //self.consume_tagged_nodes(TaggedNode { node: root, framebuffer_index: None });
   }
 
-  /// Consume a tagged node by pulling / releasing framebuffers on the fly and tagging its child as
-  /// we go deeper.
-  fn consume_tagged_nodes(&mut self, tagged_node: TaggedNode) {
-    match tagged_node.node {
-      Node::Render(render) => {
-
+  /// Treat a node hierarchy and return the index  of the framebuffer that contains the result.
+  fn treat_node(&mut self, node: Node) -> usize {
+    match node {
+      Node::Render(layer) => self.render(layer),
+      Node::Texture(texture) => self.texturize(texture),
+      Node::Composite(left, right, eq, src_fct, dst_fct) => self.composite(*left, *right, eq, src_fct, dst_fct),
     }
   }
-}
 
-/// A tagged node.
-///
-/// A tagged node is a `Node` tagged with a *framebuffer index*, which can be present or not. If
-/// it’s not present, that means that the node’s composition hasn’t occurred yet.
-struct TaggedNode<'a> {
-  node: Node<'a>,
-  framebuffer_index: Option<usize>
+  fn render(&mut self, layer: RenderLayer) -> usize {
+    let fb_index = self.pull_framebuffer();
+    let fb = &self.framebuffers[fb_index];
+    let black = [0., 0., 0., 1.];
+
+    Pipeline::new(fb, black, &[], &[], &layer.shading_commands).run();
+
+    fb_index
+  }
+
+  fn texturize(&mut self, texture: TextureLayer) -> usize {
+    let fb_index = self.pull_framebuffer();
+    let fb = &self.framebuffers[fb_index];
+
+    let black = [0., 0., 0., 1.];
+    let program = self.program.borrow();
+    let uniforms_tex = [FORWARD_SOURCE.alter(Unit::new(0))];
+    let tess_render = TessRender::from(&self.quad);
+    let tess = &[Pipe::empty().uniforms(&uniforms_tex).unwrap(tess_render.clone())];
+    let render_cmd = &[Pipe::new(RenderCommand::new(None, false, tess))];
+    let shd_cmd = &[Pipe::new(ShadingCommand::new(&program, render_cmd))];
+    let texture_set = &[&**texture];
+
+    Pipeline::new(fb, black, texture_set, &[], shd_cmd).run();
+
+    fb_index
+  }
+
+  fn composite(&mut self, left: Node, right: Node, eq: Equation, src_fct: Factor, dst_fct: Factor) -> usize {
+    let left_index = self.treat_node(left);
+    let right_index = self.treat_node(right);
+
+    assert!(left_index < self.framebuffers.len());
+    assert!(right_index < self.framebuffers.len());
+
+    let fb_index = self.pull_framebuffer();
+
+    {
+      let fb = &self.framebuffers[fb_index];
+
+      let left_fb = &self.framebuffers[left_index];
+      let right_fb = &self.framebuffers[right_index];
+
+      // compose
+      let black = [0., 0., 0., 1.];
+      let program = self.program.borrow();
+      let uniforms_left = [FORWARD_SOURCE.alter(Unit::new(0))];
+      let uniforms_right = [FORWARD_SOURCE.alter(Unit::new(1))];
+      let tess_render = TessRender::from(&self.quad);
+      let tess = &[
+        Pipe::empty().uniforms(&uniforms_left).unwrap(tess_render.clone()),
+        Pipe::empty().uniforms(&uniforms_right).unwrap(tess_render)
+      ];
+      let render_cmd = &[Pipe::new(RenderCommand::new(Some((eq, src_fct, dst_fct)), false, tess))];
+      let shd_cmd = &[Pipe::new(ShadingCommand::new(&program, render_cmd))];
+      let texture_set = &[
+        &*left_fb.color_slot,
+        &*right_fb.color_slot
+      ];
+
+      Pipeline::new(fb, black, texture_set, &[], shd_cmd).run();
+    }
+
+    // dispose both left and right framebuffers
+    self.dispose_framebuffer(left_index);
+    self.dispose_framebuffer(right_index);
+
+    fb_index
+  }
 }
