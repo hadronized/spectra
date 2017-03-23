fa5c9f6ff365ffa04ef182c93703a00fe0c8d90b
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-03-23 20:34:54 +0100

    std140 for lights.

--- src/light.rs
+++ src/light.rs
@@ -4,42 +4,39 @@ use std::default::Default;
 
 pub struct LightProp {
   pub diff: RGB,
+  _pad_0: f32,
   pub spec: RGB,
-  pub gloss: f32
+  _pad_1: f32,
+  pub gloss: f32,
+  _pad_2: [f32; 3]
 }
-
-pub struct Light<L> {
-  pub prop: LightProp,
-  pub feature: L
-}
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct LightDir(pub Direction);
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct LightPos(pub Position);
-
-pub type Dir = Light<LightDir>;
-pub type Omni = Light<LightPos>;
 
 impl LightProp {
   pub fn new(diff: RGB, spec: RGB, gloss: f32) -> Self {
     LightProp {
       diff: diff,
+      _pad_0: 0.,
       spec: spec,
-      gloss: gloss
+      _pad_1: 0.,
+      gloss: gloss,
+      _pad_2: [0., 0., 0.]
     }
   }
 }
 
 impl Default for LightProp {
   fn default() -> Self {
-    LightProp {
-      diff: RGB::new(0.6, 0.6, 0.7),
-      spec: RGB::new(0.6, 0.6, 0.7),
-      gloss: 10.
-    }
+    LightProp::new(
+      RGB::new(0.6, 0.6, 0.7),
+      RGB::new(0.6, 0.6, 0.7),
+      10.
+    )
   }
+}
+
+pub struct Light<L> {
+  pub prop: LightProp,
+  pub feature: L
 }
 
 impl<L> Light<L> {
@@ -50,3 +47,37 @@ impl<L> Light<L> {
     }
   }
 }
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct LightDir {
+  dir: Direction,
+  _pad: f32
+}
+
+impl From<Direction> for LightDir {
+  fn from(dir: Direction) -> Self {
+    LightDir {
+      dir: dir,
+      _pad: 0.
+    }
+  }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct LightPos {
+  pos: Position,
+  _pad: f32
+}
+
+impl From<Position> for LightPos {
+  fn from(pos: Position) -> Self {
+    LightPos {
+      pos: pos,
+      _pad: 0.
+    }
+  }
+}
+
+pub type Dir = Light<LightDir>;
+pub type Omni = Light<LightPos>;
+
