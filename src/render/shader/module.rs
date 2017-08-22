4dcd15df4c39c76fa0b7f43fb2235e244c396429
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-08-23 01:38:55 +0200

    First shot at no-cycle shader modules.

--- src/render/shader/module.rs
+++ src/render/shader/module.rs
@@ -11,8 +11,13 @@ use std::io::Read;
 use std::path::{Path, PathBuf};
 
 use render::shader::lang::parser;
-use render::shader::lang::syntax::{Module as SyntaxModule};
+use render::shader::lang::syntax::{Module as SyntaxModule, ModulePath};
 use sys::resource::{CacheKey, Load, LoadError, LoadResult, Store, StoreKey};
+
+// FIXME: see where to put that shit; especially, ModuleKey should be implemented with ModuleKe(ModulePath), LOL
+fn module_path_to_module_key(mp: &ModulePath) -> ModuleKey {
+  ModuleKey(mp.path.join("/") + ".spsl")
+}
 
 /// Shader module.
 ///
@@ -22,6 +27,51 @@ use sys::resource::{CacheKey, Load, LoadError, LoadResult, Store, StoreKey};
 /// disk and let everyting happen automatically for you.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Module(SyntaxModule);
+
+impl Module {
+  /// Retrieve all the modules this module depends on, without duplicates.
+  pub fn deps(&self, store: &mut Store, path: &ModulePath) -> Result<Vec<ModulePath>, DepsError> {
+    let mut deps = Vec::new();
+    self.deps_no_cycle(store, &path, &mut Vec::new(), &mut deps).map(|_| deps)
+  }
+
+  fn deps_no_cycle(&self, store: &mut Store, path: &ModulePath, parents: &mut Vec<ModulePath>, deps: &mut Vec<ModulePath>) -> Result<(), DepsError> {
+    let imports = self.0.imports.iter().map(|il| &il.module);
+
+    parents.push(path.clone());
+
+    for module_name in imports {
+      // check whether it’s already in the deps
+      if deps.contains(module_name) {
+        continue;
+      }
+
+      // check whether the module was already visited
+      if parents.contains(module_name) {
+        return Err(DepsError::Cycle(path.clone(), module_name.clone()));
+      }
+
+      // get the dependency module 
+      let module = store.get(&module_path_to_module_key(&module_name)).ok_or_else(|| DepsError::LoadError(module_name.clone()))?;
+      let r = module.borrow().deps_no_cycle(store, module_name, parents, deps)?;
+
+      deps.push(module_name.clone());
+      parents.pop();
+    }
+
+    Ok(())
+  }
+}
+
+/// Class of errors that can happen in dependencies.
+#[derive(Clone, Debug, PartialEq)]
+pub enum DepsError {
+  /// If a module’s dependencies has any cycle, the dependencies are unusable and the cycle is
+  /// returned.
+  Cycle(ModulePath, ModulePath),
+  /// There was a loading error of a module.
+  LoadError(ModulePath)
+}
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct ModuleKey(String);
