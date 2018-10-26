edd3cf5d9e95e678b690295751ad2b50fa75c047
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2018-10-26 23:49:38 +0200

    Add Block::new.

--- src/render/block.rs
+++ src/render/block.rs
@@ -1,4 +1,4 @@
-//! Render blocks.
+//! Render combinatory blocks.
 //!
 //! Render blocks are logical rendering units that have inputs and outputs. Inputs can represent
 //! vertex attributes, user-specified values, built-ins or previous blocks’ outputs.
@@ -10,9 +10,24 @@ use warmy::Res;
 use crate::render::input::Input;
 use crate::render::output::Output;
 
+/// A render block, allowing for combining blocks in order to create more complex rendering
+/// computations.
 #[derive(Clone, Debug)]
 struct Block {
   inputs: Vec<Input>,
   outputs: Vec<Output>,
   code: Res<Module>
+}
+
+impl Block {
+  /// Create a new block out of inputs, outputs and a GLSL module.
+  pub fn new<I, O>(inputs: I, outputs: O, code: Res<Module>) -> Self
+  where I: Iterator<Item = Input>,
+        O: Iterator<Item = Output> {
+    Block {
+      inputs: inputs.collect(),
+      outputs: outputs.collect(),
+      code
+    }
+  }
 }
